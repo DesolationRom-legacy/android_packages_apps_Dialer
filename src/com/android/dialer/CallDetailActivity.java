c351816117b044120aefc3480df61e4805a890d9
Martin Brabham <optedoblivion@cyngn.com>
2015-03-04 16:15:17 -0800

    Call Recording: Pause orientation handling during playback to prevent playback interruptions.
    Change-Id: I2ce162025b41d677e7942f77bf89424806c5fef9

--- src/com/android/dialer/CallDetailActivity.java
+++ src/com/android/dialer/CallDetailActivity.java
@@ -149,7 +149,7 @@ public class CallDetailActivity extends AnalyticsActivity implements ProximitySe
     private final ProximitySensorListener mProximitySensorListener = new ProximitySensorListener();
 
     private CallRecordingDataStore mCallRecordingDataStore = new CallRecordingDataStore();
-    private CallRecordingPlayer mCallRecordingPlayer = new CallRecordingPlayer();
+    private CallRecordingPlayer mCallRecordingPlayer;
 
     /** Listener to changes in the proximity sensor state. */
     private class ProximitySensorListener implements ProximitySensorManager.Listener {
@@ -237,6 +237,8 @@ public class CallDetailActivity extends AnalyticsActivity implements ProximitySe
         super.onCreate(icicle);
 
         setContentView(R.layout.call_detail);
+
+        mCallRecordingPlayer = new CallRecordingPlayer(this);
 
         mAsyncTaskExecutor = AsyncTaskExecutors.createThreadPoolExecutor();
         mInflater = (LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
